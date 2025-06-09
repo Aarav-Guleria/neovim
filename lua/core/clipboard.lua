d9223d89ff825085fc53bc84501b132d0ee814a7
Aarav-Guleria <alatus1820@gmail.com>
2025-06-09 22:52:51 +0530

    clipboard issue update

--- lua/core/clipboard.lua
+++ lua/core/clipboard.lua
@@ -1,13 +1,14 @@
+-- lua/core/clipboard.lua
 vim.g.clipboard = {
-  name = "win32yank",
+  name = "wsl-clipboard",
   copy = {
     ["+"] = "clip.exe",
-    ["*"] = "clip.exe"
+    ["*"] = "clip.exe",
   },
   paste = {
-    ["+"] = "powershell.exe -command Get-Clipboard",
-    ["*"] = "powershell.exe -command Get-Clipboard"
+    ["+"] = "powershell.exe -nologo -noprofile -command Get-Clipboard",
+    ["*"] = "powershell.exe -nologo -noprofile -command Get-Clipboard",
   },
-  cache_enabled = 1,
+  cache_enabled = 0,
 }
 
